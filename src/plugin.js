509e52b196ae3930bbf1290a67a414772e9868d9
Martti Laine <martti@codeclown.net>
2020-10-15 17:05:16 +0300

    Utilize events instead of phases

--- src/plugin.js
+++ src/plugin.js
@@ -11,29 +11,18 @@ module.exports = function licenseNoticePlugin(bundler, opts) {
     opts
   );
 
-  // This is a very simple Browserify-plugin. We push a new transformation
-  // into the bundler, but in that transformation we don't actually transform
-  // anything. We just collect the individual rows into an array. When the
-  // bundler is finished, we call `writeNotice()`. For more information, see:
-  // https://github.com/browserify/browserify-handbook#compiler-pipeline
-
   const rows = [];
 
-  bundler.pipeline.get('deps').push(
-    through.obj(
-      function write(row, enc, next) {
-        rows.push(row);
-        next();
-      },
-      function end(cb) {
-        const licenseNotice = processRows(rows);
-        if (opts.check) {
-          performCheck(licenseNotice, fs.readFileSync(opts.out, 'utf8'));
-        } else {
-          fs.writeFileSync(opts.out, licenseNotice);
-        }
-        cb();
-      }
-    )
-  );
+  bundler.on('dep', (row) => {
+    rows.push(row);
+  });
+
+  bundler.on('end', () => {
+    const licenseNotice = processRows(rows);
+    if (opts.check) {
+      performCheck(licenseNotice, fs.readFileSync(opts.out, 'utf8'));
+    } else {
+      fs.writeFileSync(opts.out, licenseNotice);
+    }
+  });
 };
